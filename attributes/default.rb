4dce942130e7ff7ebf8765b5730f4539ca2424d6
RichardWigley <richard.wigley@gmail.com>
2014-05-27 19:48:38 +0000

    set obviously wrong fqdn.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['nginx']['default_site_enabled'] = false
 # Monit_virtual_server recipe
 # HTTPD through Nginx allowing userfriendly monit.<server> address
 default['nginx']['monit_available'] = false
-default['nginx']['monit_address'] = 'monit.islay'
+default['nginx']['monit_address'] = 'monit.change.this'
 default['nginx']['monit_port'] = '2812'
 
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'richard.wigley@gmail.com'
 license          ''
 description      'Installs/Configures nginx for bcs systems.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 
 supports 'ubuntu', '>= 12.04'
