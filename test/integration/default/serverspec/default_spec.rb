5a3ea575763e03119b9b95ded9653913ab8d0e19
RichardWigley <richard.wigley@gmail.com>
2014-05-27 15:19:40 +0000

    kibana tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,15 @@ describe 'bcs_nginx::default' do
   it 'installed nginx version' do
     expect(package 'nginx').to be_installed
   end
+
+  describe 'kibana site' do
+    describe file '/etc/nginx/sites-available/kibana' do
+      it { should be_file }
+    end
+
+    describe file '/etc/nginx/sites-enabled/kibana' do
+      it { should be_file }
+      it { should be_linked_to '/etc/nginx/sites-available/kibana' }
+    end
+  end
 end
