8590dac6aa29c5bfac93e5c70da1f71f67da442d
RichardWigley <richard.wigley@gmail.com>
2014-05-27 16:27:36 +0000

    added empty sanity test

---
+++ test/integration/empty/serverspec/default_spec.rb
@@ -0,0 +1,26 @@
+require 'serverspec'
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+describe 'bcs_nginx::default' do
+
+  describe 'monit site' do
+    describe file '/etc/nginx/sites-available/monit' do
+      it { should_not be_file }
+    end
+
+    describe file '/etc/nginx/sites-enabled/monit' do
+      it { should_not be_file }
+    end
+  end
+
+  describe 'kibana site' do
+    describe file '/etc/nginx/sites-available/kibana' do
+      it { should_not be_file }
+    end
+
+    describe file '/etc/nginx/sites-enabled/kibana' do
+      it { should_not be_file }
+    end
+  end
+end
