c26afcc2791cbe0c484269ceb9189f65ece3db75
Richard <richard.wigley@gmail.com>
2017-04-30 12:21:39 +0100

    License updated

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'bcs_nginx'
 maintainer       'BCS-io'
 maintainer_email 'richard.wigley@gmail.com'
-license          ''
+license          'MIT'
 description      'Installs/Configures nginx for bcs systems.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
@@ -11,6 +11,7 @@ issues_url       'https://github.com/BCS-io-provision/bcs_nginx/issues'
 
 supports 'ubuntu', '>= 14.04'
 
-%w(apt chef_nginx).each do |cb|
-  depends cb
-end
+depends 'apt'
+depends 'chef_nginx', '>= 6.0.0'
+
+chef_version '>= 12.5' if respond_to?(:chef_version)
