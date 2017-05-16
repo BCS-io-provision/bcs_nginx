b78ee879eed629a97fdd99025d4d9818665ba95e
Richard <richard.wigley@gmail.com>
2017-05-16 14:47:55 +0100

    Remove cloned resource
      - resource was repeating the script found in chef_nginx
        - chef_nginx::package ln 48 also sets up NGINX service

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,3 @@
 include_recipe 'chef_nginx::default'
 include_recipe 'bcs_nginx::monit_site'
 include_recipe 'bcs_nginx::kibana_site'
-
-service 'nginx' do
-  supports status: true, restart: true, reload: true
-  action [:start, :enable]
-end
