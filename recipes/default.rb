dd2183d19d2a75701b955550216c8f9d7999b4ef
Richard <richard.wigley@gmail.com>
2017-07-03 18:58:26 +0100

    Add http_ssl_module recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,6 @@
 # Copyright 2014, BCS-io
 #
 include_recipe 'chef_nginx::default'
+include_recipe 'chef_nginx::http_ssl_module'
 include_recipe 'bcs_nginx::monit_site'
 include_recipe 'bcs_nginx::kibana_site'
